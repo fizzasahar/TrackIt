189cc37b925268102a445d1e10fffac69fafb207
fizzasahar <fizzasahar44>
2025-04-30 18:46:49 +0500

    Completed

--- server/server.mjs
+++ server/server.mjs
@@ -15,7 +15,7 @@ app.use(
     cors({
         origin: ['http://localhost:5174',
             'http://localhost:5173',
-            'https://track-it-one-phi.vercel.app/',
+            'https://track-it-one-phi.vercel.app',
 
         ],
         methods: ['GET', 'PUT', 'POST', 'DELETE','PATCH'],
